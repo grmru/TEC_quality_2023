b959ba42a4063aa2632a8b47c404d2c245b802bd
Oleg Starshinov <ostarshinoff@gmail.com>
2023-02-18 11:52:24 +0300

    Добавил свою папку, написал простой код, записал в gitignore исключения

---
+++ oleg/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Test
+{
+    class Program
+    {
+       static void Main(string[] args)
+       { 
+        Console.WriteLine("Введите своё имя: ");
+        string Name = Console.ReadLine();
+        Console.WriteLine("Сколько тебе лет?");
+        string Age = Console.ReadLine();
+        FindYear(Age);
+       }
+       static void FindYear(string Age)
+       {
+        int YourAge = int.Parse(Age);
+        int  CurrentYear = DateTime.Now.Year;
+        int result = CurrentYear - YourAge;
+        Console.WriteLine($"Ты родился в {result}");
+        
+       }
+
+    }
+}
